c85d29c311fcbb4d71d26966c801ac1659663aae
yvtapir <murathanselimoglu6116@hotmail.com>
2022-09-23 12:25:49 +0300

    Update test_migration.py

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -16,6 +16,8 @@ def test_migration(
     gov,
     user,
     prepare_trade_factory,
+    ib_token,
+    want_price_feed,
     RELATIVE_APPROX
 ):
     # Deposit to the vault and harvest
@@ -26,7 +28,8 @@ def test_migration(
     assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
 
     # migrate to a new strategy
-    new_strategy = strategist.deploy(Strategy, vault)
+    new_strategy = strategist.deploy(Strategy, vault, ib_token, 1, want_price_feed) # wftm
+    #new_strategy = strategist.deploy(Strategy, vault, ib_token, 3, want_price_feed) # usdc
     vault.migrateStrategy(strategy, new_strategy, {"from": gov})
     assert (
         pytest.approx(new_strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX)
