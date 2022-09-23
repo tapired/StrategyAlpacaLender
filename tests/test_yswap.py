04bfe93fc4f1adc40f73e96b4f4dd42e44b7ab23
yvtapir <murathanselimoglu6116@hotmail.com>
2022-09-23 12:26:07 +0300

    Update test_yswap.py

--- tests/test_yswap.py
+++ tests/test_yswap.py
@@ -1,9 +1,11 @@
 from brownie import Contract, Wei
-from zrx_swap import zrx_swap
 import brownie
-
+from eth_abi import encode_single, encode_abi
+from brownie.convert import to_bytes
+from eth_abi.packed import encode_abi_packed
 import pytest
 import eth_utils
+
 
 def test_profitable_harvest(
     chain,
@@ -18,8 +20,9 @@ def test_profitable_harvest(
     ymechs_safe,
     prepare_trade_factory,
     alpaca_token,
-    zrx_swapper,
-    gov
+    unirouter,
+    gov,
+    multicall_swapper
 ):
 
     # Deposit to the vault
@@ -36,17 +39,29 @@ def test_profitable_harvest(
 
     # make profit
     chain.sleep(86400 * 5)
-    chain.mine(1)
+    chain.mine(100)
+    print(strategy.estimatedTotalAssets())
 
-    yswap(chain, strategy, token, zrx_swapper, alpaca_token, ymechs_safe, gov, prepare_trade_factory, trade_factory)
+    yswap(
+        chain,
+        strategy,
+        token,
+        unirouter,
+        alpaca_token,
+        ymechs_safe,
+        gov,
+        prepare_trade_factory,
+        trade_factory,
+        multicall_swapper
+    )
 
     tx = strategy.harvest({"from": strategist})
-    print(tx.events)
     assert tx.events["Harvested"]["profit"] > 0
     print(strategy.estimatedTotalAssets())
 
-
-def test_remove_trade_factory_token(strategy, gov, trade_factory, alpaca_token, prepare_trade_factory):
+def test_remove_trade_factory_token(
+    strategy, gov, trade_factory, alpaca_token, prepare_trade_factory
+):
     assert strategy.tradeFactory() == trade_factory.address
     assert alpaca_token.allowance(strategy.address, trade_factory.address) > 0
 
@@ -55,7 +70,10 @@ def test_remove_trade_factory_token(strategy, gov, trade_factory, alpaca_token, 
     assert strategy.tradeFactory() != trade_factory.address
     assert alpaca_token.allowance(strategy.address, trade_factory.address) == 0
 
-def test_harvest_reverts_without_trade_factory(strategy, gov, user, vault, token, chain, amount):
+
+def test_harvest_reverts_without_trade_factory(
+    strategy, gov, user, vault, token, chain, amount
+):
     # Deposit to the vault
     user_balance_before = token.balanceOf(user)
     token.approve(vault.address, amount, {"from": user})
@@ -68,27 +86,64 @@ def test_harvest_reverts_without_trade_factory(strategy, gov, user, vault, token
     with brownie.reverts("Trade factory must be set."):
         strategy.harvest()
 
+
 ###################################################################################################
 
 
-def yswap(chain, strategy, token, zrx_swapper, alpaca_token, ymechs_safe, gov, prepare_trade_factory, trade_factory):
-    strategy.claimRewards({"from":gov})
-    # locked profit
-    chain.sleep(86400)
-    chain.mine(1)
+def yswap(
+    chain,
+    strategy,
+    token,
+    unirouter,
+    alpaca_token,
+    ymechs_safe,
+    gov,
+    prepare_trade_factory,
+    trade_factory,
+    multicall_swapper
+):
+    strategy.claimRewards({"from": gov})
 
     token_in = alpaca_token
     token_out = token
     receiver = strategy.address
     amount_in = token_in.balanceOf(strategy)
-    assert (amount_in > 0)
+    assert amount_in > 0
 
     asyncTradeExecutionDetails = [strategy, token_in, token_out, amount_in, 1]
-    swap_data = zrx_swap.getDefaultQuote(str(token), str(alpaca_token), amount_in)
+    optimizations = [["uint8"], [5]]
+    a = optimizations[0]
+    b = optimizations[1]
+
+    calldata = token_in.approve.encode_input(unirouter, amount_in)
+    t = createTx(token_in, calldata)
+    a = a + t[0]
+    b = b + t[1]
+
+    path = [token_in.address, token_out.address]
+    calldata = unirouter.swapExactTokensForTokens.encode_input(
+        amount_in, 0, path, multicall_swapper, 2 ** 256 - 1
+    )
+    t = createTx(unirouter, calldata)
+    a = a + t[0]
+    b = b + t[1]
+
+    expectedOut = unirouter.getAmountsOut(amount_in, path)[1]
+
+    calldata = token_out.transfer.encode_input(receiver, expectedOut)
+    t = createTx(token_out, calldata)
+    a = a + t[0]
+    b = b + t[1]
+
+    transaction = encode_abi_packed(a, b)
 
     trade_factory.execute["tuple,address,bytes"](
         asyncTradeExecutionDetails,
-        zrx_swapper.address,
-        swap_data,
-        {"from": ymechs_safe}
+        multicall_swapper.address,
+        transaction,
+        {"from": ymechs_safe},
     )
+
+def createTx(to, data):
+    inBytes = eth_utils.to_bytes(hexstr=data)
+    return [["address", "uint256", "bytes"], [to.address, len(inBytes), inBytes]]
